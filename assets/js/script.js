caace26744d0a713d5f36ae33f1ac1510ef2f0b4
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-09-18 17:50:52 -0400

    Adding in info box, buttons, and quiz box to HTML, CSS, and JS

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// variables up top
+const start_btn = document.querySelector (".start_btn button");
+const details_box = document.querySelector ("details_box");
+
+const next_btn = details_box.querySelector (".next_btn button")
+
+
+
+
+
+start_btn.onclick = ()=>{
+    details_list.ClassList.add ("")
+}
+

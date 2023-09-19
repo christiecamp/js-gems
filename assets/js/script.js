656f157b5f71d9403c9b425474a274c176784c2b
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-09-19 00:43:46 -0400

    details & quiz box html, css, and js updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,14 @@ const next_btn = details_box.querySelector (".next_btn button")
 
 
 
-
+// when start button is clicked - it shows details box
 start_btn.onclick = ()=>{
-    details_list.ClassList.add ("")
+    details_box.ClassList.add ("activeInfo");
 }
 
+// when next button is clicked - it shows quiz box, calls questions function, starts timer function
+
+next_btn.onclick = ()=>{
+    details_box.ClassList.remove ("activeInfo"); //hides details box
+    
+}

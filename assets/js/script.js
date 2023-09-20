851c38f547342d1f65a96cb842abc6fc7d65c1e4
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-09-20 11:55:06 -0400

    changing js click

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,15 @@ const next_btn = details_box.querySelector (".next_btn button")
 
 
 // when start button is clicked - it shows details box
-start_btn.onclick = ()=> {
-    details_box.ClassList.add ("activeInfo");
-}
+// start_btn.onclick = ()=> {
+    // details_box.ClassList.add ("activeInfo");
+// }
 
 // when next button is clicked - it shows quiz box, calls questions function, starts timer function
 
-next_btn.onclick = ()=> {
-    details_box.ClassList.remove ("activeInfo"); //hides details box
-    quiz_box.ClassList.add ("activeQuiz"); // shows quiz box
-}
+// next_btn.onclick = ()=> {
+   //  details_box.ClassList.remove ("activeInfo"); //hides details box
+   //  quiz_box.ClassList.add ("activeQuiz"); // shows quiz box
+// }
 
 

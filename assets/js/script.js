cc7e561f7b8aaa205a08227cc135177cf833b536
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-09-20 11:44:51 -0400

    updating HTML, CSS, and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,15 @@ const next_btn = details_box.querySelector (".next_btn button")
 
 
 // when start button is clicked - it shows details box
-start_btn.onclick = ()=>{
+start_btn.onclick = ()=> {
     details_box.ClassList.add ("activeInfo");
 }
 
 // when next button is clicked - it shows quiz box, calls questions function, starts timer function
 
-next_btn.onclick = ()=>{
+next_btn.onclick = ()=> {
     details_box.ClassList.remove ("activeInfo"); //hides details box
-    
+    quiz_box.ClassList.add ("activeQuiz"); // shows quiz box
 }
+
+

83ce030476328826b1fcec709a66888f0337164e
christiecamp <christiecamphoto@gmail.com>
2024-02-03 23:23:53 -0500

    updated gemscore.js

--- assets/js/gemscores.js
+++ assets/js/gemscores.js
@@ -1,29 +1,28 @@
 //highscores
-const gems = document.querySelector('.gems');
-const highGemscore = document.querySelector('#highgemscore');
-const back = document.querySelector('#back-btn');
-const clear = document.querySelector('#clear-btn');
+const gems = document.querySelector("#gems");
+const highGemscore = document.querySelector("#highgemscore");
+const back = document.querySelector("#back");
+const clear = document.querySelector("#clear");
+
+//local storage
+const allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+console.log(allScores);
+if (allScores !== null) {
+  for (var i = 0; i < allScores.length; i++) {
+    const createLi = document.createElement("li");
+    createLi.textContent = allScores[i].name + " " + allScores[i].score;
+    gems.appendChild(createLi);
+  }
+}
 
 //back button
-back.addEventListener('click', function() {
-    window.location.replace('index.html');
+back.addEventListener("click", function () {
+  window.location.replace("index.html");
 });
 
 //clear button
-clear.addEventListener('click', function() {
-    localStorage.clear();
-    location.reload();
+clear.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
 });
-
-const scores = localStorage.getItem('scores');
-scores = JSON.parse(scores);
-
-if (scores !== null) {
-    for (var i = 0; i < scores.length; i++) { //const or var?
-        const add = document.createElement('li');
-        add.setAttribute('id', 'Li');//check once questions are created - id or class
-        add.textContent = scores[i].initials + " " + scores[i].gemscore; //check once questions are created
-
-        scores.appendChild(add); //check once questions are created
-    }
-};

f457fdf8dea7449e87d344936535af067a17f93c
christiecamp <christiecamphoto@gmail.com>
2024-02-03 23:30:40 -0500

    updating styling

--- assets/js/gemscores.js
+++ assets/js/gemscores.js
@@ -1,28 +1,28 @@
 //highscores
-const gems = document.querySelector("#gems");
-const highGemscore = document.querySelector("#highgemscore");
-const back = document.querySelector("#back");
-const clear = document.querySelector("#clear");
+const gems = document.querySelector('#gems');
+const highGemscore = document.querySelector('#highgemscore');
+const back = document.querySelector('#back');
+const clear = document.querySelector('#clear');
 
 //local storage
-const allScores = localStorage.getItem("allScores");
+const allScores = localStorage.getItem('allScores');
 allScores = JSON.parse(allScores);
 console.log(allScores);
 if (allScores !== null) {
   for (var i = 0; i < allScores.length; i++) {
-    const createLi = document.createElement("li");
-    createLi.textContent = allScores[i].name + " " + allScores[i].score;
+    const createLi = document.createElement('li');
+    createLi.textContent = allScores[i].name + ''+ allScores[i].score;
     gems.appendChild(createLi);
   }
 }
 
 //back button
-back.addEventListener("click", function () {
-  window.location.replace("index.html");
+back.addEventListener('click', function () {
+  window.location.replace('./index.html');
 });
 
 //clear button
-clear.addEventListener("click", function () {
+clear.addEventListener('click', function () {
   localStorage.clear();
   location.reload();
 });

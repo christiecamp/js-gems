f32f97259f110342d7d0864581e52dbb4e9e7f5a
christiecamp <christiecamphoto@gmail.com>
2024-02-03 22:49:04 -0500

    working through questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,58 +1,71 @@
-const container = document.querySelector('.container');
-const gems = document.querySelector('.gems');
-const timer = document.querySelector('#timer');
-const enter = document.querySelector('#enter-btn');
-
 //questions
-const input = [
+const questions = [
     {
-        title: 'question1',
-        choices: [],
-        answer: 'answer1'
+        title: 'High quality opal is more valuable than diamonds',
+        choices: ['true', 'false'],
+        answer: 'trye'
     },
     {
-        title: 'question2',
-        choices: [],
-        answer: 'answer2'
+        title: 'which gem has the nickname of "water sapphire"',
+        choices: ['iolite', 'blue topaz', 'tanzanite', 'sapphire'],
+        answer: 'iolite'
     },
     {
-        title: 'question3',
-        choices: [],
-        answer: 'answer3'
+        title: `according to historic legend - what was Cleopatra's favorite gemstone?`,
+        choices: ['emerald', 'peridot', 'sapphire', 'pearl'],
+        answer: 'peridot'
     },
     {
-        title: 'question4',
-        choices: [],
-        answer: 'answer4'
+        title: 'rubies and sapphires come frome the same mineral',
+        choices: ['true', 'false'],
+        answer: 'true'
     },
     {
-        title: 'question5',
-        choices: [],
-        answer: 'answer5'
+        title: `which fruit's seeds was garnet named after?`,
+        choices: ['grape', 'apple', 'orange','pomegranate'],
+        answer: 'pomegranate'
     },
     {
-        title: 'question6',
-        choices: [],
-        answer: 'answer6'
+        title: `tanzanite was named by Tiffany & Co. in honor of the east African country where it was discovered in the 1960s - what is it's scientific name?`,
+        choices: ['zircon', 'spinel', 'tanzanite', 'zoicite'],
+        answer: 'zoicite'
     },
     {
-        title: 'question7',
-        choices: [],
-        answer: 'answer7'
+        title: 'how long does it take to grow a cultured pearl',
+        choices: ['1-3 years', '4-6 years', '7-9 years', '10-12 years'],
+        answer: '1-3 years'
     },
     {
-        title: 'question8',
-        choices: [],
-        answer: 'answer8'
+        title: `zircon isn't natural gemstone?`,
+        choices: ['true', 'false'],
+        answer: 'false'
     },
     {
-        title: 'question9',
-        choices: [],
-        answer: 'answer9'
+        title: `the phenomenon of cat's eye (or tiger's eye) is called`,
+        choices: ['asterism', 'adularescence', 'chatoyancy', 'play of color'],
+        answer: 'chatoyancy'
     },
     {
-        title: 'question10',
-        choices: [],
-        answer: 'answer10'
+        title: 'which gemstone is the birthstone for the month of October?',
+        choices: ['pearl', 'alexandrite', 'opal', 'diamond'],
+        answer: 'opal'
     },
 ];
+
+var score = 0;
+var questionIndex = 0;
+
+//declared variables
+const container = document.querySelector('#container');
+const timer = document.querySelector('#timer');
+const gems = document.querySelector('#gems');
+const enter = document.querySelector('#enter-btn');
+
+//timer
+var timeLeft = 76;
+//interval time
+var holdInterval = 0;
+//penalty time
+var penalty = 10;
+//create new element
+var ulCreate = document.createElement('ul');
